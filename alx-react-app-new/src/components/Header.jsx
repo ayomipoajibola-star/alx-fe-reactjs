2059dff508e5ab9adc5db6c6ff190a462a7ec0e2
Ayomipo Ajibola <ayomipo.ajibola@gmail.com>
2026-01-18 20:25:47 +0000

    Added inline CSS to all components

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -4,6 +4,9 @@ function Header() {
             <h2 style={{ color: 'blue', fontSize: '24px' }}>{props.name}</h2>
             <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
+            <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '20px' }}>
+  <h1>My Favorite Cities</h1>
+</header>
         </div>
     );
 }
