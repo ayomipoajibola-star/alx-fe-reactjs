aaa774ffe88a4a57743e6726b68a72c665af3ea0
Ayomipo Ajibola <ayomipo.ajibola@gmail.com>
2026-01-18 21:46:45 +0000

    Setup Vite React project with pages

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
+import Navbar from './Components/Navbar';
 import Home from './Home';
 import About from './About';
 import Services from './Services';
