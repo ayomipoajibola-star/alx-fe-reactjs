f2e027c3f7f9c37cb0a1d4b8d051f40b78ce5a3f
Ayomipo Ajibola <ayomipo.ajibola@gmail.com>
2026-01-18 20:23:43 +0000

    Added inline CSS to all components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', textAlign: 'left', fontFamily: 'Arial, sans-serif', lineHeight: '1.6' }}>
+            <h2 style={{ color: 'darkgreen' }}>Welcome to My City App</h2>
+            <p>Here you can explore some of the best cities around the world.</p>
+        </main>
+    );
+}
+
+export default MainContent;
