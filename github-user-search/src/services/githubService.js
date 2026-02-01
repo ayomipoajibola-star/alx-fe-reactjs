a78e197d159f30328de5c320d84dc04daa1ff408
Ayomipo Ajibola <ayomipo.ajibola@gmail.com>
2026-02-01 20:02:18 +0000

    Enhancing UI & Advanced Search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com";
+const GITHUB_API_URL = "https://api.github.com/search/users";
 
 export const fetchAdvancedUsers = async (
   username,
@@ -10,20 +10,24 @@ export const fetchAdvancedUsers = async (
 ) => {
   let query = "";
 
-  if (username) query += `${username} `;
-  if (location) query += `location:${location} `;
-  if (minRepos) query += `repos:>=${minRepos}`;
+  if (username) {
+    query += username;
+  }
 
-  const response = await axios.get(
-    `${BASE_URL}/search/users`,
-    {
-      params: {
-        q: query.trim(),
-        page,
-        per_page: 10,
-      },
-    }
-  );
+  if (location) {
+    query += ` location:${location}`;
+  }
 
-  return response.data;
+  if (minRepos) {
+    query += ` repos:>=${minRepos}`;
+  }
+
+  try {
+    const response = await axios.get(
+      `${GITHUB_API_URL}?q=${query}&page=${page}&per_page=10`
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
