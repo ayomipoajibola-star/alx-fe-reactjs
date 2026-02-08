9fedecdcc50a9e4db8fedad2036d78ee6c1abc78
Ayomipo Ajibola <ayomipo.ajibola@gmail.com>
2026-02-08 17:54:16 +0000

    Style UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
+      <div>
+        <UserProfile />
+      </div>
+
       <h1>Vite + React</h1>
       <h1 className="text-4xl font-bold text-blue-600">
       Tailwind is working 🚀
@@ -28,11 +34,12 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
   )
 }
-
 export default App
