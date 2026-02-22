d50198efc41987bf9611335911c470a436046208
Ayomipo Ajibola <ayomipo.ajibola@gmail.com>
2026-02-22 19:54:31 +0000

    Implement advanced routing: nested, dynamic, protected routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,51 +3,50 @@ import { useState } from "react";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Profile from "./components/Profile";
-import ProfileDetails from "./pages/ProfileDetails";
-import ProfileSettings from "./pages/ProfileSettings";
 import BlogPost from "./pages/BlogPost";
+import Profile from "./components/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  // Simple authentication simulation
+  // Simple authentication state (required for protected routes)
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/blog/1">Blog Post</Link> |{" "}
-        <Link to="/login">Login</Link>
-      </nav>
+      <div>
+        {/* Navigation */}
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/profile">Profile</Link> |{" "}
+          <Link to="/blog/1">Blog Post</Link> |{" "}
+          <Link to="/login">Login</Link>
+        </nav>
 
-      <Routes>
-        {/* Basic Route */}
-        <Route path="/" element={<Home />} />
+        {/* Routes */}
+        <Routes>
+          {/* Home */}
+          <Route path="/" element={<Home />} />
 
-        {/* Login */}
-        <Route
-          path="/login"
-          element={<Login setIsAuthenticated={setIsAuthenticated} />}
-        />
+          {/* Login */}
+          <Route
+            path="/login"
+            element={<Login setIsAuthenticated={setIsAuthenticated} />}
+          />
 
-        {/* Protected + Nested Routes */}
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <Profile />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+          {/* Protected Route with Nested Routes handled inside Profile.jsx */}
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute isAuthenticated={isAuthenticated}>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Dynamic Route */}
-        <Route path="/blog/:id" element={<BlogPost />} />
-      </Routes>
+          {/* Dynamic Route */}
+          <Route path="/blog/:id" element={<BlogPost />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
